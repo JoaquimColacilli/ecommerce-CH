0c3db5db9ad82b813cb852b1e33364239ad59ace
Joaquim Augusto Colacilli <joaquimcolacilli9@gmail.com>
2021-11-22 20:51:45 -0300

    estilando css de item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,32 +4,17 @@ import { Link } from "react-router-dom";
 const Item = ({ id, nombre, desc, precio, imagen }) => {
   console.log(id);
   return (
-    // <div className="container">
-    //   <div className="row">
-    //     <div className="col-md">
-    //       <div className="card text-center">
-    //         <div className="card-body">
-    //           <h4 className="card-title">{nombre}</h4>
-    //           <p className="card-text">{desc}</p>
-    //           <span>
-    //             <img src={imagen} alt={nombre} className={"small-img"} />
-    //           </span>
-    //           <p className="card-text">${precio}</p>
-
-    //           <Link to={`/producto/${id}`} className="btn btn-primary">
-    //             Ver Más
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="col-lg-4 paddingCardItem">
       <div className="card">
         <div className="card-body">
           <h4 className="card-title">{nombre}</h4>
           <img src={imagen} alt={nombre} className="img-fluid" />
-          <p className="card-text">${precio}</p>
+          <p className="card-text mt-2" style={{ fontSize: "18px" }}>
+            <span style={{ fontWeight: "bold" }}>Precio: </span>${precio}
+          </p>
+          <p className="card-text" style={{ fontSize: "16px" }}>
+            {desc}
+          </p>
           <button type="button" className="btn btn-outline-dark">
             <Link
               to={`/producto/${id}`}
